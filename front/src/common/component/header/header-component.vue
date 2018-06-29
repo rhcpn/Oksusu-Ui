9b74dc5473aa0c999cbb822778fd0047c8d62a21
Jihyun <almalibreolo@gmail.com>
2018-06-29 14:42:57 +0900

    메뉴 컴포넌트 작업완료 - 디자인 버그 수정완료 - header 컴포넌트로 분리

---
+++ front/src/common/component/header/header-component.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="fixed-top-cont">
+    <div class="top-left">
+      <v-toolbar-side-icon id="foldingBtn" @click.stop="isFolding = !isFolding"></v-toolbar-side-icon>
+    </div>
+    <div class="top-right">
+      <ul class="util-menu">
+        <li>
+          <v-badge left>
+            <span slot="badge" class="bg-red">62</span>
+            <v-icon color="white" class="md-18">notifications</v-icon>
+          </v-badge>
+        </li>
+        <li>
+          <v-menu offset-y>
+            <v-btn flat small slot="activator"><v-icon left color="white" class="md-18">person</v-icon>User info<v-icon right color="white" class="md-18">expand_more</v-icon></v-btn>
+            <!--<v-list>-->
+            <!--<v-list-tile v-for="(item, index) in items" :key="index" @click="">-->
+            <!--<v-list-tile-title>{{ item.title }}</v-list-tile-title>-->
+            <!--</v-list-tile>-->
+            <!--</v-list>-->
+          </v-menu>
+        </li>
+        <li>
+          <v-btn flat class="btn-util-logout"><v-icon color="white">power_settings_new</v-icon></v-btn>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header-component',
+  props: {
+    menuFolding: {
+      type: Boolean,
+      require: true
+    }
+  },
+  data () {
+    return {
+      isFolding: this.menuFolding
+    }
+  },
+  watch: {
+    isFolding: {
+      immediate: true,
+      handler (val, oldVal) {
+        setTimeout(this.menuWrapperHandler, 100)
+      }
+    }
+  },
+  methods: {
+    menuWrapperHandler () {
+      let wrapper = $('.wrapper')
+      if (this.isFolding === true) {
+        wrapper.addClass('slim-type')
+      } else {
+        wrapper.removeClass('slim-type')
+      }
+      this.$eventHub.$emit('menu-folding', this.isFolding)
+    }
+  }
+}
+</script>
--- front/src/common/component/menu/gnb-component.vue
+++ front/src/common/component/menu/gnb-component.vue
@@ -2,7 +2,7 @@
   <aside>
     <h1 class="logo"><a href="#none">T-CORE</a></h1>
     <div class="gnb">
-      <!-- Depth1 -->
+      <!-- Menu Depth1 -->
       <ul class="gnb-menu" v-if="menus.length">
         <menu-child-component v-for="menu in menus" :key="menu.path" :data="menu"></menu-child-component>
       </ul>
@@ -19,7 +19,6 @@ import menuChild from '@/common/component/menu/menu-child-component'
 
 Vue.component('menu-child-component', menuChild)
 
-const SLIM_MENU_WIDTH = 50
 export default {
   name: 'gnb-component',
   created: function () {
@@ -35,41 +34,17 @@ export default {
     width: {
       type: Number,
       default: 300
-    },
-    folding: {
-      type: Boolean,
-      default: false
     }
   },
   data () {
     return {
       wrapperWidth: this.width,
-      isFolding: this.folding,
-      foldingIcon: '▶',
       menus: []
-    }
-  },
-  watch: {
-    isFolding: {
-      immediate: true,
-      handler (val, oldVal) {
-        console.log('is Folding event;', val, oldVal)
-        if (val === true) {
-          this.wrapperWidth = SLIM_MENU_WIDTH
-          this.foldingIcon = '▶'
-        } else {
-          this.wrapperWidth = this.width
-          this.foldingIcon = '◀'
-        }
-      }
     }
   },
   methods: {
     init: function () {
       this.menus = this.$router.options.routes
-    },
-    menuFoldingHandler: function () {
-      this.isFolding = !this.isFolding
     }
   }
 }
