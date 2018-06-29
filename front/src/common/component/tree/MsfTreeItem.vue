72ea0602f0fe6f6c435799c26877b2ab911e8781
hyunseokjung <firesapa1@mobigen.com>
2018-06-29 15:51:45 +0900

    트리 소스 적용

---
+++ front/src/common/component/tree/MsfTreeItem.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="treeitem">
+    <div :style="{ width: data.depth * 10 + 'px'}"></div>
+    <div class="expanded" :style="{cursor : hasChild(data) ? 'pointer' : ''}" v-on:click="onClick">{{ hasChild(data)?
+      expanded ? '-' : '+' : ''}}
+    </div>
+    <!--<div class="checkbox" v-bind:class="isChecked" v-on:click="checkClick"></div>-->
+    <div class="item" v-on:click="checkClick(data)">{{ label }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'msf-tree-item',
+  props: {
+    data: Object,
+    checked: Boolean,
+    expanded: Boolean,
+    depth: Number,
+    label: String,
+    halfChecked: Boolean
+  },
+  methods: {
+    hasChild: function (data) {
+      return data && data.hasOwnProperty('children') && data.children && data.children.length > 0
+    },
+    checkClick: function () {
+      let bool = !this.data.checked
+      this.data.checked = bool
+      console.log(this.data)
+      this.$emit('checkclick', this.data)
+    },
+    onClick: function () {
+      let item = this.data
+      if (!this.hasChild(item)) {
+        return
+      }
+      console.log(item)
+      item.expanded = !item.expanded
+      this.$emit('expand', item)
+    }
+  },
+  computed: {
+    isChecked: function () {
+      if (this.checked) {
+        return 'checked'
+      } else {
+        if (this.halfChecked) {
+          return 'halfchecked'
+        } else {
+          return ''
+        }
+      }
+    }
+
+  }
+}
+</script>
+<style>
+  .treeitem {
+    width: 100%;
+  }
+
+  .treeitem div {
+    display: inline-block;
+    height: 30px;
+  }
+  .treeitem .expanded {
+    width: 20px;
+  }
+  .treeitem .checkbox {
+    width: 20px;
+    height: 20px;
+    border:1px solid #CCCCCC
+  }
+  .checked {
+    background-color: #00FF00
+  }
+  .halfchecked {
+    background-color: #FFFF00
+  }
+  .treeitem .item {
+    cursor: pointer
+  }
+</style>
---
+++ front/src/site/resource/ResourceInfra.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="layout row gap-06">
+    <div class="flex md3">
+      <div class="panel-wrap">
+        <v-tabs class="header-tabs"
+                v-model="active"
+                color="none"
+                slider-color="none"
+        >
+          <v-tab
+            v-for="n in 2"
+            :key="n"
+            ripple
+          >
+            Item {{ n }}
+          </v-tab>
+          <v-tab-item
+            v-for="n in 2"
+            :key="n"
+          >
+            <v-card flat>
+              <v-card-text><msf-tree :source="source"></msf-tree></v-card-text>
+            </v-card>
+          </v-tab-item>
+        </v-tabs>
+
+      </div>
+    </div>
+
+    <div class="flex md9">
+      <div class="panel-wrap">
+        <div class="panel-header">
+          <ul class="breadcrumbs left">
+            <li><a href="#none">SK</a></li>
+            <li><a href="#none">Data center1</a></li>
+            <li><a href="#none">Data center2</a></li>
+          </ul>
+        </div>
+        <div class="panel-body">
+          <v-tabs class="sub-tabs"
+                  v-model="active"
+                  color="none"
+                  slider-color="none"
+          >
+            <v-tab
+              v-for="n in 2"
+              :key="n"
+              ripple
+            >
+              Item {{ n }}
+            </v-tab>
+            <v-tab-item
+              v-for="n in 2"
+              :key="n"
+            >
+              <v-card flat>
+                <v-card-text>{{ n }}</v-card-text>
+              </v-card>
+            </v-tab-item>
+          </v-tabs>
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+import sampleData from '../../asset/json/getResource.json'
+import MsfTree from '../../common/component/tree/MsfTree.vue'
+export default {
+  name: 'resource-infra',
+  components: {MsfTree},
+  data () {
+    return {
+      source: [] // 전체 데이터
+    }
+  },
+  methods: {},
+  created () {
+    this.source = sampleData
+  }
+}
+</script>
