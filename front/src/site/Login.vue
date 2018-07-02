c1ef911e3bf3ac19e71ec9db01bd8a08ee776791
lyj <eojoo00@gmail.com>
2018-07-02 18:04:01 +0900

    login 버그 수정

--- front/src/site/Login.vue
+++ front/src/site/Login.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="text-md-center" style="width: 100%">
     <h2>Login</h2>
-    <form @submit.prevent="onSubmit(email, password)">
-      <!--<input type="text" v-model="email" placeholder="Email Address">-->
+    <form @submit.prevent="onSubmit(id, password)">
       <input type="text" v-model="id" placeholder="ID">
       <input type="password" v-model="password" placeholder="Password">
       <input type="submit" value="Login">
@@ -15,7 +14,6 @@ export default {
   name: 'Login',
   data () {
     return {
-      // email: '',
       id: '',
       password: '',
       msg: ''
