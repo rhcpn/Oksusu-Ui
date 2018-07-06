982e809a955bfa9b8aa4a849fef60ef4a82efb3b
lyj <eojoo00@gmail.com>
2018-07-06 14:59:46 +0900

    ResourceInfra.vue - 아이콘 디자인 변경, 목록보기/상면보기 버튼

--- front/src/site/resource/ResourceInfra.vue
+++ front/src/site/resource/ResourceInfra.vue
@@ -32,8 +32,8 @@
           <ul class="breadcrumbs left" v-if="selectDepthArray.length" >
             <li v-for="depth in selectDepthArray" :key="depth.id"><a href="#none">{{depth.name}}</a></li>
           </ul>
-          <v-btn icon class="btn-md-tool" v-bind:class="{ on: viewType == 'list' }" @click="viewType = 'list'"><v-icon>view_module</v-icon></v-btn>
-          <v-btn icon class="btn-md-tool" v-bind:class="{ on: viewType == 'img' }" @click="viewType = 'img'"><v-icon>view_list</v-icon></v-btn>
+          <v-btn class="btn-md-tool" v-if="resultInfo.type === 'floor' || resultInfo.type === 'room'" v-bind:class="{ on: viewType == 'list' }" @click="viewType = 'list'"><v-icon>view_list</v-icon></v-btn>
+          <v-btn class="btn-md-tool" v-if="resultInfo.type === 'floor' || resultInfo.type === 'room'" v-bind:class="{ on: viewType == 'img' }" @click="viewType = 'img'"><v-icon>view_module</v-icon></v-btn>
           <div class="right">
             <div class="input-srh w200">
               <v-icon>search</v-icon>
@@ -97,6 +97,9 @@ export default {
       this.getDepthArray(item)
       this.selectDepthArray.push(item)
       console.log(this.selectDepthArray)
+
+      // 목록 / 상면도 보기 초기화
+      this.viewType = 'list'
     },
     tabItemClick: function (type) {
       this.$refs.resourceData.resultDataTabGrid(type)
